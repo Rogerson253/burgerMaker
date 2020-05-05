876715ad8e6fa34a9a03c7a50756cf05fcef9c35
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-05-04 21:36:19 -0500

    created buttons and text field for burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var orm = require("../config/orm.js");
 
+// Creates burgers
 router.post("/api/burger", function (req, res) {
   const burger_name = req.body.burger_name;
   orm.insertOne(burger_name, function (data) {
@@ -15,15 +16,16 @@ router.post("/api/burger", function (req, res) {
   });
 });
 
+// Reads Burgers
 router.get("/", function (req, res) {
   orm.selectAll(function (data) {
     res.render("index", { burgers: data });
   });
 });
 
+// Updates burgers
 router.put("/api/burger/:id", function (req, res) {
   const id = req.params.id;
-
   orm.updateOne(id, function (data) {
     if (data.affectedRows === 1) {
       res.json({
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+// Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 7000;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
 
 // Express Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(express.json());
 
 // Handlebars Middleware
